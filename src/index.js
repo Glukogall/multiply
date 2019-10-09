3986a70ffc83c41db7a393142c33372c5f6de9ac
Galka <gallkogluk@gmail.com>
2019-10-09 18:44:34 +0400

    feat: add answer for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let firstArr = first.split('').reverse();
+  let secondArr = second.split('').reverse();
+  
+
+  let steck = [];
+
+  for (let i = 0; i < firstArr.length; i++) {
+    for (let j = 0; j < secondArr.length; j++) {
+      let number = firstArr[i] * secondArr[j];
+    
+      if ((steck[i + j])){
+        steck[i + j]=steck[i + j] + number
+      }
+      else{
+        steck[i + j]=number
+      }
+      
+    }
+  }
+
+  for (let i = 0; i < steck.length; i++) {
+    let num = steck[i] % 10;
+    let carry = Math.floor(steck[i] / 10);
+    steck[i] = num;
+
+    if (steck[i + 1])
+      steck[i + 1] += carry;
+    else if (carry != 0)
+      steck[i + 1] = carry;
+  }
+
+  let rrr=steck.reverse().join('')
+  console.log(rrr)
+  return rrr;
+  
 }
